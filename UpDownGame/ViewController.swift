40936d69b1d648f437e9c2fffb105406bf026eff
WhiteKr <hanjisu2215g@gmail.com>
2021-09-13 13:47:53 +0900

    messages to alert

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -30,6 +30,18 @@ class ViewController: UIViewController {
         sliderValueLabel.text = String(intValue)
     }
     
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: nil,
+                                      message: message,
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) {
+            (action) in self.reset()
+        }
+        
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
     @IBAction func touchUpHitButton(_ sender: UIButton) {
         let hitValue: Int = Int(slider.value)
         slider.value = Float(hitValue)
@@ -38,10 +50,10 @@ class ViewController: UIViewController {
         tryCountLabel.text = "\(tryCount) / 5"
         
         if randomValue == hitValue {
-            print("YOU HIT!!")
+            showAlert(message: "YOU HIT!!")
             reset()
         } else if tryCount >= 5 {
-            print("You lose...")
+            showAlert(message: "You lose...")
             reset()
         } else if randomValue > hitValue {
             slider.minimumValue = Float(hitValue)
