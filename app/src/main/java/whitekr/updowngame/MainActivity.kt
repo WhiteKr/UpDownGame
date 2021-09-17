a905e0b167e7bc6a2be8876f8b0d22e2c24fa4a6
WhiteKr <hanjisu2215g@gmail.com>
2021-09-17 22:27:26 +0900

    base view

--- app/src/main/java/whitekr/updowngame/MainActivity.kt
+++ app/src/main/java/whitekr/updowngame/MainActivity.kt
@@ -2,10 +2,17 @@ package whitekr.updowngame
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		val slider: SeekBar = findViewById(R.id.main_seekBar_slider)
+		val sliderCount: TextView = findViewById(R.id.main_textView_sliderCount)
+		val min: TextView = findViewById(R.id.main_textView_min)
+		val max: TextView = findViewById(R.id.main_textView_max)
 	}
 }
