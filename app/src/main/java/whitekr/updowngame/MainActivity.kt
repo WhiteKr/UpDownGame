6c5057fe89014a118623c9e203d1d521be13b931
WhiteKr <hanjisu2215g@gmail.com>
2021-09-17 22:52:50 +0900

    random goal & reset

--- app/src/main/java/whitekr/updowngame/MainActivity.kt
+++ app/src/main/java/whitekr/updowngame/MainActivity.kt
@@ -4,27 +4,36 @@ import android.os.Build
 import android.os.Bundle
 import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+	var goal: Int = 0
+
+	private val min: Int = 0
+	private val max: Int = 30
+
+	lateinit var slider: SeekBar
+	lateinit var sliderCount: TextView
+	lateinit var sMin: TextView
+	lateinit var sMax: TextView
+	lateinit var submitBtn: Button
+
 	@RequiresApi(Build.VERSION_CODES.O)
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
-		val slider: SeekBar = findViewById(R.id.main_seekBar_slider)
-		val sliderCount: TextView = findViewById(R.id.main_textView_sliderCount)
-		val sMin: TextView = findViewById(R.id.main_textView_min)
-		val sMax: TextView = findViewById(R.id.main_textView_max)
+		slider = findViewById(R.id.main_seekBar_slider)
+		sliderCount = findViewById(R.id.main_textView_sliderCount)
+		sMin = findViewById(R.id.main_textView_min)
+		sMax = findViewById(R.id.main_textView_max)
+		submitBtn = findViewById(R.id.main_button_submit)
 
-		val (min: Int, max: Int) = intArrayOf(0, 30)
-		slider.min = min
-		slider.max = max
-		sMin.text = slider.min.toString()
-		sMax.text = slider.max.toString()
-		slider.progress = max / 2
-		sliderCount.text = slider.progress.toString()
+		reset()
 
 		slider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
 			override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
@@ -37,5 +46,26 @@ class MainActivity : AppCompatActivity() {
 			override fun onStopTrackingTouch(p0: SeekBar?) {
 			}
 		})
+
+		submitBtn.setOnClickListener {
+			val n: Int = slider.progress
+			if (n == goal) {
+				reset()
+				Toast.makeText(this, "정답 ㅊㅊ", Toast.LENGTH_SHORT).show()
+			}
+		}
+	}
+
+	@RequiresApi(Build.VERSION_CODES.O)
+	fun reset() {
+		goal = (Math.random() * (max - min + 1)).toInt() + min
+		Log.e("goal", goal.toString())
+
+		slider.min = min
+		slider.max = max
+		sMin.text = slider.min.toString()
+		sMax.text = slider.max.toString()
+		slider.progress = max / 2
+		sliderCount.text = slider.progress.toString()
 	}
 }
